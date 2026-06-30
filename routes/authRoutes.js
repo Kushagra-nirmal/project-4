d1cbd7e5afdfaa72f94242da9c2972545980a0ae
Kushagra-Nirmal <012345nirmal@gmail.com>
2026-06-30 14:58:19 +0530

    Initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,2 +1,11 @@
 const express = requires("express");
 const authMiddleware = require("../middleware/authMiddleware");
+const { register, login, profile, logout } = require("../controllers/authController");
+const router = express.Router();
+
+router.post("/register",register);
+router.post("/login",login);
+router.get("/profile",profile);
+router.get("/logout", logout);
+
+module.exports = router;
